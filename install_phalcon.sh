98c94f5473d62602efb5d8e9e1ed8d6007e6964b
Sid Roberts <sid@sidroberts.co.uk>
2017-09-22 06:34:22 +0900

    Updated Phalcon install script.

--- install_phalcon.sh
+++ install_phalcon.sh
@@ -4,4 +4,4 @@ git clone -q --depth=1 https://github.com/phalcon/cphalcon.git -b phalcon-v$1
 
 cd cphalcon/ext/
 
-export CFLAGS="-g3 -O1 -std=gnu90 -Wall -DZEPHIR_RELEASE=1"; phpize && ./configure --enable-phalcon && make --silent -j4 && make --silent install && phpenv config-add ../unit-tests/ci/phalcon.ini
+export CFLAGS="-g3 -O1 -std=gnu90 -Wall"; phpize &> /dev/null && ./configure --silent --enable-phalcon &> /dev/null && make --silent &> /dev/null && make --silent install && phpenv config-add ../unit-tests/ci/phalcon.ini
