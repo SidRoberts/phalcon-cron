252182f1e49b1bae6f1fc2fdd9446945ad78945e
Sid Roberts <sid@sidroberts.co.uk>
2019-03-17 13:41:05 +0900

    Used install script from https://github.com/Codeception/phalcon-demo

--- install_phalcon.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-git clone -q --depth=1 https://github.com/phalcon/cphalcon.git -b v$1
-
-cd cphalcon/ext/
-
-export CFLAGS="-g3 -O1 -std=gnu90 -Wall"; phpize &> /dev/null && ./configure --silent --enable-phalcon &> /dev/null && make --silent &> /dev/null && make --silent install && phpenv config-add ../unit-tests/ci/phalcon.ini
