ae1bb300de5b79f2e3bbc3893de031caacff0acc
Sid Roberts <sid@sidroberts.co.uk>
2019-03-24 10:06:18 +0900

    Added triple = for strict comparison.

--- src/Job.php
+++ src/Job.php
@@ -52,14 +52,14 @@ abstract class Job extends Injectable implements \Sid\Cron\JobInterface
     {
         $processID = pcntl_fork();
 
-        if ($processID == -1) {
+        if ($processID === -1) {
             throw new Exception(
                 "Failed to fork process."
             );
         }
 
         // This is the child process.
-        if ($processID == 0) {
+        if ($processID === 0) {
             // @codeCoverageIgnoreStart
             $this->runInForeground();
 
